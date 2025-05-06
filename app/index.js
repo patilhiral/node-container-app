89830792c12ab2bda8c67e20eebfa87be2cb571f
Panchal <Hiral.Panchal@sse.com>
2025-05-06 21:34:47 +0100

    change the code and workflow file

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my demo app");
+  res.send("Welcome to my demo app1");
 });
 app.listen(3000, (req, res) => {
   console.log("Listening to port 3000 ");
