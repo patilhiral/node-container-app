0e84b5b82aca73501c60271323300b8eb91d54db
Panchal <Hiral.Panchal@sse.com>
2025-05-08 14:36:42 +0100

    code changes

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my demo app1");
+  res.send("Welcome to my demo app");
 });
 app.listen(3000, (req, res) => {
   console.log("Listening to port 3000 ");
